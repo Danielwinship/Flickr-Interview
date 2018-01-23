45f083248851018423368ebf64d3506b6de3f828
Daniel Winship <danielwinship@icloud.com>
2018-01-23 06:34:00 -0500

    building out model to call flickr API

---
+++ Flickr Interview/Model/FlickrPhotoModel.swift
@@ -0,0 +1,26 @@
+//
+//  FlickrPhotoModel.swift
+//  Flickr Interview
+//
+//  Created by Daniel Winship on 1/23/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import Foundation
+
+struct  FlickrPhotoModel {
+    
+    let id: Int
+    let owner: String
+}
+
+
+
+//"https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=f43946e9e7d270dd9b6883e9e4e44b09&per_page=40&format=json&nojsoncallback=1"
+
+
+                                //Secret      //ID
+//"https://www.flickr.com/photos/43039595@N02/24965633177/sizes/q/"
+
+
+//https://www.flickr.com/photos/130982849@N05/39805370732/sizes/t/
---
+++ Flickr Interview/Services/FlickrService.swift
@@ -0,0 +1,21 @@
+//
+//  FlickrService.swift
+//  Flickr Interview
+//
+//  Created by Daniel Winship on 1/23/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import Foundation
+
+class FlickrService {
+    
+    static let instance = FlickrService()
+    
+    var flickrUrl = [FlickrPhotoModel]()
+    
+    func getUrl() {
+        
+    }
+    
+}
